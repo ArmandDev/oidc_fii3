44bf3f3baefa243e816d9c28aa35c69a0146647d
IustinAchitenei <113167994+IustinAchitenei@users.noreply.github.com>
2026-03-28 17:00:50 +0000

    storage size

--- main.tf
+++ main.tf
@@ -88,16 +88,48 @@ resource "aws_security_group" "cloudpulse_sg" {
   description = "Allow SSH, HTTP, and App ports"
   vpc_id      = aws_vpc.cloudpulse.id
   # Standard SSH & HTTP
-
-  # Custom Application Ports
   ingress {
-    description = "Grafana / Apps"
+    description = "SSH"
     from_port   = 22
-    to_port     = 9999
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 8089
+    to_port     = 8089
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Custom Application Ports
+  ingress {
+    description = "Grafana / Apps"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # --- PRIVATE ACCESS (Self-Referencing) ---
+  # These ports are only reachable BY the instances inside this SG.
+
+  ingress {
+    description = "Allow all internal traffic between cluster nodes"
+    from_port   = 3000
+    to_port     = 9999
+    protocol    = "tcp"
+    self        = true
+  }
 
   egress {
     from_port   = 0
@@ -338,6 +370,13 @@ resource "aws_instance" "observability" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.cloudpulse_sg.id]
   private_ip             = "10.0.0.20"
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+    encrypted             = true
+  }
 
   user_data_replace_on_change = true
 
