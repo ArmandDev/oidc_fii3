5b98fc33af6bd871de7c728bf472944f13674fd0
IustinAchitenei <113167994+IustinAchitenei@users.noreply.github.com>
2026-03-28 12:56:54 +0000

    3001

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_security_group" "cloudpulse_sg" {
 
   ingress {
     description = "Allow all internal traffic between cluster nodes"
-    from_port   = 3000
+    from_port   = 3001
     to_port     = 9999
     protocol    = "tcp"
     self        = true
