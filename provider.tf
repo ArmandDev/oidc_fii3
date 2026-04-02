bea669f98facfdeb584dcc99fd20ec88e88a20c4
Armand <47137816+ArmandDev@users.noreply.github.com>
2026-04-02 13:01:47 +0000

    aa

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "aa-741853493541-us-east-1-an "
+    bucket  = "aa-741853493541-us-east-1-an"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
