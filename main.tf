11dfda5f9757bdfb4ebfff68365a6ac8708e22fb
Armand <47137816+ArmandDev@users.noreply.github.com>
2026-04-02 13:11:21 +0000

    aa

--- main.tf
+++ main.tf
@@ -397,7 +397,7 @@ tags = {
 resource "aws_instance" "observability" {
   provider               = aws.main
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "c7i-flex.large"
+  instance_type          = "t3.medium"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.cloudpulse_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.cloudpulse.name
